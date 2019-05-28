02048b65cb0576c33d8d8f1d34e3a5e08f8c0403
Charlie Thames <charliethames@Charlies-MacBook-Pro.local>
2019-05-28 16:39:58 -0500

    5/28

--- footer.php
+++ footer.php
@@ -1,88 +1,79 @@
+<style>
+	.footertop {
+		display: grid;
+		grid-template-columns: repeat(3,1fr);
+		background-color: var(--color-2);
+		color: white;
+		padding: 0 45px;
+		width: 100vw;
+		height: 225px;
+		grid-row: 1;
+	}
 
-		<style>
-#footers {
-	position: relative;
-	display: grid;
-	grid-template-rows: 270px 30px;
-	bottom: 0px;
-	margin: 0px;
-	width: 100vw;
-	height: 300px;
-}
+	.footertop1, .footertop2, .footertop3 {
+		text-align: center;
+		margin:20px 40px;
+	}
 
-.footertop {
-	display: grid;
-	grid-template-columns: repeat(3,1fr);
-	background-color: slategray;
-	color: white;
-	padding: 0 45px;
-	width: 100vw;
-	height: 225px;
-	grid-row: 1;
-}
+	.footertop1 {
+		grid-column: 1;
+	}
 
-.footertop1, .footertop2, .footertop3 {
-	text-align: center;
-	margin:20px 40px;
-}
+	.footertop1header,.footertop2header,.footertop3header {
+		font-size: 28px;
+		font-weight: bold;
+		padding-bottom:10px;
+	}
 
-.footertop1 {
-	grid-column: 1;
-}
+	.footertop2 {
+		grid-column: 2;
+	}
 
-.footertop1header,.footertop2header,.footertop3header {
-	font-size: 28px;
-	font-weight: bold;
-	padding-bottom:10px;
-}
+	.footertop3 {
+		grid-column: 3;
+	}
 
-.footertop2 {
-	grid-column: 2;
-}
+	.footerbottom {
+		color: var(--main-txt-color);
+		text-align: center;
+		width: 100vw;
+		height: 25px;
+		padding: 5px;
+		grid-row: 2;
+		background-color: var(--color-5);
+	}
 
-.footertop3 {
-	grid-column: 3;
-}
+</style>
 
-.footerbottom {
-	color: black;
-	text-align: center;
-	width: 100vw;
-	height: 25px;
-	padding: 5px;
-	grid-row: 2;
-}
-		</style>
-
-			<div class="footertop">
-				<div class="footertop1">
-					<div class="footertop1header">
-						About
-					</div>
-					<div class="footertop1body">
-						Imperial Wire was founded in  sfj jsf ijs dd d jklsd saoihoauehtuskdfngkajs nasujnask jfnaifn sdfn sf asoif jskmrli s fsdf sdf 
-					</div>
-				</div>
-				<div class="footertop2">
-					<div class="footertop2header">
-						Contact
-					</div>
-					<div class="footertop2body">
-						<p>21050 Commerce Blvd, Rogers, MN 55374</p>
-						<p>763-391-7798</p>
-						<p>Shop: 7:00AM to 2:30AM (CST)</p>
-						<p>Office: 7:00AM to 4:30PM (CST)</p>
-					</div>
-				</div>
-				<div class="footertop3">
-					<div class="footertop3header">
-						Quick Inquiry
-					</div>
-					<div class="footertop3body">
-						Sign up for our mailing list to stay up to date on our latest offerings and specials
-					</div>
-				</div>
-			</div>
-			<div class="footerbottom">
-				© 2019 ImperialWire. All Rights Reserved. - Address - Clients
-			</div>
+<div class="footertop">
+	<div class="footertop1">
+		<div class="footertop1header">
+			About
+		</div>
+		<div class="footertop1body">
+			Imperial Wire was founded in  sfj jsf ijs dd d jklsd saoihoauehtuskdfngkajs nasujnask jfnaifn sdfn sf asoif jskmrli s fsdf sdf 
+		</div>
+	</div>
+	<div class="footertop2">
+		<div class="footertop2header">
+			Contact
+		</div>
+		<div class="footertop2body">
+			<p>21050 Commerce Blvd, Rogers, MN 55374</p>
+			<p>763-391-7798</p>
+			<p>Shop: 7:00AM to 2:30AM (CST)</p>
+			<p>Office: 7:00AM to 4:30PM (CST)</p>
+		</div>
+	</div>
+	<div class="footertop3">
+		<div class="footertop3header">
+			Quick Inquiry
+		</div>
+		<div class="footertop3body">
+			Sign up for our mailing list to stay up to date on our latest offerings and specials
+		</div>
+	</div>
+</div>
+<div class="footerbottom">
+	© 2019 ImperialWire. All Rights Reserved. - Address - Clients
+</div>
